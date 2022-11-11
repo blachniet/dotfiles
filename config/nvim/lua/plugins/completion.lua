b9a5efd80e11bf72ebfa77377ddd32041abaad1f
Brian Lachniet <blachniet@gmail.com>
2022-11-10 20:58:23 -0500

    nvim: Call new default_capabilities in nvim-cmp
    See <https://github.com/hrsh7th/nvim-cmp/pull/1243>

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -102,7 +102,7 @@ cmp.setup.cmdline(':', {
 
 -- Setup lspconfig.
 local nvim_lsp = require("lspconfig")
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- rust_analyzer: Do not include this server here. It would conflict with the
 -- setup performed by simrat39/rust-tools.nvim.
