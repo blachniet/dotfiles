fec2e2ae7e95848e27cb4fec4dff988056b6e7a5
Brian Lachniet <blachniet@gmail.com>
2022-12-13 21:38:31 -0500

    nvim: Convert markdown ftplugin to lua

---
+++ config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+vim.opt_local.conceallevel = 2       -- Hide formatting modifiers
+vim.opt_local.spell        = true    -- Enable spell checking
+vim.opt_local.spelllang    = 'en_us'
+vim.opt_local.tabstop      = 4
+vim.opt_local.shiftwidth   = 4
+vim.opt_local.expandtab    = true
