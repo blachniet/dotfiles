bf286d9899eaab1212a123c7ad89cfab35456c12
Brian Lachniet <blachniet@gmail.com>
2022-12-17 11:25:41 -0500

    fish: Add $HOME/.local/bin/ to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,8 @@
 starship init fish | source
 
-
 set -xg EDITOR nvim
 set -xg GPG_TTY (tty)
+set --path --prepend PATH $HOME/.local/bin/
 
 # FZF
 set -xg FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
