4360bcae6c157ff629a263dfbe02089a3a5aac09
Brian Lachniet <blachniet@gmail.com>
2022-12-17 11:56:24 -0500

    nvim: Use localleader in ftplugins

--- config/nvim/ftplugin/rust.lua
+++ config/nvim/ftplugin/rust.lua
@@ -1,9 +1,9 @@
 local bufopts = { silent=true, buffer=true }
 
-vim.keymap.set('n', '<leader>cb', '<cmd>Cbuild<cr>G', bufopts)
-vim.keymap.set('n', '<leader>crr', '<cmd>Crun<cr>G', bufopts)
-vim.keymap.set('n', '<leader>crt', '<cmd>Cruntarget<cr>G', bufopts)
-vim.keymap.set('n', '<leader>rta', '<cmd>RustTest!<cr>G', bufopts)
-vim.keymap.set('n', '<leader>rtc', '<cmd>RustTest<cr>G', bufopts)
+vim.keymap.set('n', '<localleader>cb', '<cmd>Cbuild<cr>G', bufopts)
+vim.keymap.set('n', '<localleader>crr', '<cmd>Crun<cr>G', bufopts)
+vim.keymap.set('n', '<localleader>crt', '<cmd>Cruntarget<cr>G', bufopts)
+vim.keymap.set('n', '<localleader>rta', '<cmd>RustTest!<cr>G', bufopts)
+vim.keymap.set('n', '<localleader>rtc', '<cmd>RustTest<cr>G', bufopts)
 vim.keymap.set('n', 'ged', '<cmd>RustOpenExternalDocs', bufopts)
 
