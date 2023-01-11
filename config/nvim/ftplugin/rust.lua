c533a4685ea4b2910ca67b0c98c9225fd8c0c318
Brian Lachniet <blachniet@gmail.com>
2023-01-10 22:27:01 -0500

    nvim: Convert rust.lua to rust.vim

--- config/nvim/ftplugin/rust.lua
+++
@@ -1,9 +0,0 @@
-local bufopts = { silent=true, buffer=true }
-
-vim.keymap.set('n', '<localleader>cb', '<cmd>Cbuild<cr>G', bufopts)
-vim.keymap.set('n', '<localleader>crr', '<cmd>Crun<cr>G', bufopts)
-vim.keymap.set('n', '<localleader>crt', '<cmd>Cruntarget<cr>G', bufopts)
-vim.keymap.set('n', '<localleader>rta', '<cmd>RustTest!<cr>G', bufopts)
-vim.keymap.set('n', '<localleader>rtc', '<cmd>RustTest<cr>G', bufopts)
-vim.keymap.set('n', 'ged', '<cmd>RustOpenExternalDocs', bufopts)
-
---
+++ config/nvim/ftplugin/rust.vim
@@ -0,0 +1,7 @@
+nnoremap <LocalLeader>cb <Cmd>Cbuild<CR>G
+nnoremap <LocalLeader>crr <Cmd>Crun<CR>G
+nnoremap <LocalLeader>crt <Cmd>Cruntarget<CR>G
+nnoremap <LocalLeader>rta <Cmd>RustTest!<CR>G
+nnoremap <LocalLeader>rtc <Cmd>RustTest<CR>G
+nnoremap ged <Cmd>RustOpenExternalDocs<CR>
+
