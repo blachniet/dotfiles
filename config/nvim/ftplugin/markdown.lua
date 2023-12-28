999b6affc4bf590c5295cbd28b732d23928d64d5
Brian Lachniet <blachniet@gmail.com>
2023-12-28 11:44:42 -0500

    nvim: Replace markdown settings lua with vim

--- config/nvim/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-vim.opt_local.conceallevel = 2       -- Hide formatting modifiers
-vim.opt_local.spell        = true    -- Enable spell checking
-vim.opt_local.spelllang    = 'en_us'
-vim.opt_local.tabstop      = 4
-vim.opt_local.shiftwidth   = 4
-vim.opt_local.expandtab    = true
-vim.opt_local.wrap         = true
---
+++ config/nvim/ftplugin/markdown.vim
@@ -0,0 +1,8 @@
+setlocal conceallevel=2  " Hide formatting modifiers
+setlocal spell           " Enable spell checking
+setlocal spelllang=en_us
+setlocal tabstop=4
+setlocal shiftwidth=4
+setlocal expandtab
+setlocal wrap
+setlocal nonumber
