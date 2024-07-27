af12e096e5ac23ca60c7c9352c2ce94dc044e023
Brian Lachniet <blachniet@gmail.com>
2024-07-26 20:12:47 -0400

    fish: Add atuin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,17 @@
-starship init fish | source
-
 set -xg EDITOR nvim
 set -xg GPG_TTY (tty)
 set --path --prepend PATH $HOME/.local/bin/
 
-# FZF
-set -xg FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
-set -xg FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-
 set -x KUBECONFIG $HOME/.kube/config
 for c in $HOME/.kube/config-*; set --path --append KUBECONFIG $c; end
+
+if status is-interactive
+
+  starship init fish | source
+  atuin init fish | source
+
+  ## FZF
+  #set -xg FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+  #set -xg FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+end
