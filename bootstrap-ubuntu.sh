7a3db8bca4303823276ce0f1b5c4d7a064805ede
Brian Lachniet <blachniet@gmail.com>
2024-08-30 23:22:00 -0400

    linux: Make bootstrap script executable

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/bash
+
+set -ex
+
 apt-get -y update
 apt-get -y install \
   build-essential \
@@ -18,3 +22,5 @@ mv nvim-linux64 /opt/nvim
 # Install vim-plug for Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+/opt/nvim/bin/nvim +PlugInstall +qall
