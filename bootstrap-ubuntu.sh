daebdd9d3759f9959cdd376059b6cb765f646c74
Brian Lachniet <blachniet@gmail.com>
2024-08-30 23:26:31 -0400

    linux: Remove Neovim plug install from bootstrap

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -22,5 +22,3 @@ mv nvim-linux64 /opt/nvim
 # Install vim-plug for Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-/opt/nvim/bin/nvim +PlugInstall +qall
