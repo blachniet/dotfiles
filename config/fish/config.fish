9d779087e14b9254ebd9692f89786edaffeb1403
Brian Lachniet <blachniet@gmail.com>
2025-10-26 09:14:44 -0400

    fish: Source rustup configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,7 @@ for c in $HOME/.kube/config-*; set --path --append KUBECONFIG $c; end
 if status is-interactive
 
   /opt/homebrew/bin/brew shellenv | source
+  source "$HOME/.cargo/env.fish"
   starship init fish | source
   atuin init fish | source
   fnm env --use-on-cd --shell fish | source
