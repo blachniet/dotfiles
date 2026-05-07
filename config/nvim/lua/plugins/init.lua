b115524fea02d139b5a985f7a92c2fdeadec8300
Brian Lachniet <blachniet@gmail.com>
2026-05-06 21:23:42 -0400

    nvim: Add lazy loading for venn.vim and vim-test

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -21,7 +21,15 @@ return {
   { 'lewis6991/gitsigns.nvim', opts = {} },
 
   -- Testing
-  'vim-test/vim-test',
+  { 'vim-test/vim-test',
+    keys = {
+      { '<leader>tn', '<cmd>TestNearest<cr>', desc = 'Test Nearest' },
+      { '<leader>tf', '<cmd>TestFile<cr>', desc = 'Test File' },
+      { '<leader>ts', '<cmd>TestSuite<cr>', desc = 'Test Suite' },
+      { '<leader>tl', '<cmd>TestLast<cr>', desc = 'Test Last' },
+      { '<leader>tv', '<cmd>TestVisit<cr>', desc = 'Test Visit' },
+    },
+  },
 
   -- Focus/zen mode
   { 'folke/zen-mode.nvim',
@@ -38,7 +46,32 @@ return {
   },
 
   -- ASCII diagrams
-  'jbyuki/venn.nvim',
+  { 'jbyuki/venn.nvim',
+    keys = {
+      { '<leader>v', function()
+        local venn_enabled = vim.inspect(vim.b.venn_enabled)
+        if venn_enabled == "nil" then
+          vim.b.venn_enabled = true
+          vim.cmd[[setlocal ve=all]]
+          -- draw a line on HJKL keystokes
+          vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+          -- draw a box by pressing "f" with visual selection
+          vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+        else
+          vim.api.nvim_buf_del_keymap(0, "n", "J")
+          vim.api.nvim_buf_del_keymap(0, "n", "K")
+          vim.api.nvim_buf_del_keymap(0, "n", "L")
+          vim.api.nvim_buf_del_keymap(0, "n", "H")
+          vim.api.nvim_buf_del_keymap(0, "v", "f")
+          vim.cmd[[setlocal ve=]]
+          vim.b.venn_enabled = nil
+        end
+      end, desc = 'Toggle Venn' },
+    },
+  },
 
   -- AI completion
   'github/copilot.vim',
